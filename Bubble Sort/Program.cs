4188b11e9094b06c78af2975710c3babbcbb0bbb
Nima Poshtiban <poshtibannima@gmail.com>
2021-09-03 15:59:14 +0430

    Add project files.

---
+++ Bubble Sort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Bubble_Sort
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 2, 3, 45, 6 };
+            BubbleSort(array);
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int partIndex = array.Length-1;partIndex>0;partIndex--)
+            {
+                for (int i = 0; i <partIndex; i++)
+                {
+                    if (array[i] > array[i+1])
+                    {
+                        Swap(array, i, i+1);
+                    }
+                }
+            }
+            // this part is not part of the algorithm
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+            {
+                return;
+            }
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
