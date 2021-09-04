af842047109a1744331e75904729343ca54ff108
Nima Poshtiban <poshtibannima@gmail.com>
2021-09-04 19:16:39 +0430

    added data Structure , and added linked list class

---
+++ Data Structures/DataStructures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Data_Structures.Linked_List;
+namespace Data_Structures
+{
+    public class DataStructures
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList();
+
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast(3);
+
+
+        }
+    }
+}
---
+++ Data Structures/Linked List/LinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures.Linked_List
+{
+    public class LinkedList
+    {
+        private class Node
+        {
+            private int value;
+            public Node next;
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+        private Node first;
+        private Node last;
+        public void AddLast(int item)
+        {
+            var node = new Node(item);
+
+            if (first == null)
+                first = last = node;
+            else
+            {
+                last.next = node;
+                last = node;
+            }
+
+        }
+    }
+}
+
+
