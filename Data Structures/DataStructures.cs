e57500fce2445a95da5c707739a087750420e46c
Nima Poshtiban <poshtibannima@gmail.com>
2021-09-07 16:48:45 +0430

    bug fixed

--- Data Structures/DataStructures.cs
+++ Data Structures/DataStructures.cs
@@ -6,16 +6,7 @@ namespace Data_Structures
     {
         static void Main(string[] args)
         {
-            var tree = new Tree.BinarySearchTree();
-
-            tree.Insert(5);
-            tree.Insert(1);
-            tree.Insert(3);
-            tree.Insert(6);
-            tree.Insert(9);
-
-            tree.TraverseInOrder();
-            Console.WriteLine(tree.Min());
+            
         }
     }
 }
