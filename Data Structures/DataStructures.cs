ca666f07eee37a864beec4130a6a42ea0c9179f8
Nima Poshtiban <poshtibannima@gmail.com>
2021-09-07 12:48:39 +0430

    added Tree Data Structure

--- Data Structures/DataStructures.cs
+++ Data Structures/DataStructures.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using Data_Structures.Stack;
 namespace Data_Structures
 {
     public class DataStructures
     {
         static void Main(string[] args)
         {
-          
+           
         }
     }
 }
---
+++ Data Structures/Tree/Tree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures.Tree
+{
+    public class Tree
+    {
+        private class Node
+        {
+            public int value;
+            public Node leftChild;
+            public Node rightChild;
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+        private Node root;
+        public void Insert(int value)
+        {
+            if (root == null)
+            {
+                root = new Node(value);
+                return;
+            }
+            Node current = root;
+
+            while (true)
+            {
+                if (value < current.value)
+                {
+                    if (current.leftChild == null)
+                    {
+                        current.leftChild = new Node(value);
+                        break;
+                    }
+                    current = current.leftChild;
+                }
+                else
+                {
+                    if (current.rightChild == null)
+                    {
+                        current.rightChild = new Node(value);
+                        break;
+                    }
+                    current = current.rightChild;
+                }
+            }
+        }
+    }
+}
